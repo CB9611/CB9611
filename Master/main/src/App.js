e9aa410386690355baf0e660abc875b7aa1b49ab
Cameron Branch <cameron.branch@selu.edu>
2021-04-28 01:49:46 -0500

    Just the beginning :)

---
+++ Master/main/src/App.js
@@ -0,0 +1,17 @@
+/* Style imports */
+import './styles/App.css';
+
+/* Function imports */
+import Router from 'react-router-dom';
+
+/* Component imports */
+
+function App() {
+  return (
+    <div className="App">
+      Hey
+    </div>
+  );
+}
+
+export default App;
---
+++ PACKAGES.md
@@ -0,0 +1,4 @@
+npm i react-scripts --s
+npm i react-router-dom --s
+npm install --save reactstrap react react-dom
+npm install --save bootstrap
